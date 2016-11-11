9e46899573d19a15100b7f99438305c9f485f18c
ErikWitt <erik.witt1@googlemail.com>
2016-11-11 15:11:22 +0100

    initialization bug

--- baqend/initializeRating.js
+++ baqend/initializeRating.js
@@ -1,13 +1,12 @@
-exports.call = function(db, data, req) {
-  return db.send(new db.message.TruncateBucket('Rating')).then(function () {
-    return db.Product.find().resultList();
-  }).then(function(products){
-    products.forEach(function(product) {
-      var rating = new db.Rating();
-      rating.id = product.key;
-      rating.ratings = 0;
-      rating.stars = 0;
-      rating.save();
+exports.call = function (db, data, req) {
+  return db.Product.find().resultList()
+    .then(function (products) {
+      products.forEach(function (product) {
+        var rating = new db.Rating();
+        rating.id = product.key;
+        rating.ratings = 0;
+        rating.stars = 0;
+        rating.save();
+      });
     });
-  });
 };
