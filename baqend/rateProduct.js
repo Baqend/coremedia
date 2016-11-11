f9a786542c0edf90210aae20452acb71fc408d7a
ErikWitt <erik.witt1@googlemail.com>
2016-11-11 12:22:19 +0100

    Bugfix in product details

--- baqend/rateProduct.js
+++ baqend/rateProduct.js
@@ -1,5 +1,4 @@
 exports.call = function(db, data, req) {
-  db.log.info('new rating', {data: data})
   return db.Rating.load(data.productKey).then(function(r) {
     return r.optimisticSave(function(rating) {
       var n = rating.ratings + 1;
